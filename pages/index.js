8228de4b0afe5bce71a43b0b679b2f6df5b7f7fe
fekerineamar <fekerineamar@gmail.com>
2023-03-22 12:43:29 +0100

    upload

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function MyPage() {
+  const [prompt, setPrompt] = useState("")
+  const [answer, setAnswer] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    setIsLoading(true)
+    setAnswer('')
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ prompt: prompt })
+    })
+    const data = await response.json()
+    setAnswer(data.text.trim())
+    setIsLoading(false)
+  }
+
+  function handleChange(e) {
+    setPrompt(e.target.value)
+  }
+
+  return (
+    <div className="container">
+      <h1 style={{textAlign:"center"}}>Chat With AI</h1>
+      <form className="our-form" onSubmit={handleSubmit}>
+        <input className="prompt-field" type="text" onChange={handleChange} />
+        <button className="prompt-button">Generate</button>
+      </form>
+
+      {isLoading && <div className="loading-spinner" style={{textAlign:"center"}}></div>}
+
+      <div className="answer-area">{answer}</div>
+    </div>
+  )
+}
